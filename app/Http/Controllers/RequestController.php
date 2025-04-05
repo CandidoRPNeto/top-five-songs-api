71b7eaba29fc5791a522b420d2a9e8a58553f28e
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-04-05 00:18:57 -0300

    start routes

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Request;
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    public function send()
+    {
+        //
+    }
+
+    public function acceptRequest()
+    {
+        //
+    }
+
+    public function refuseRequest()
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Songs;
+use Illuminate\Http\Request;
+
+class SongController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Songs $songs)
+    {
+        //
+    }
+
+    public function update(Request $request, Songs $songs)
+    {
+        //
+    }
+
+    public function destroy(Songs $songs)
+    {
+        //
+    }
+}
